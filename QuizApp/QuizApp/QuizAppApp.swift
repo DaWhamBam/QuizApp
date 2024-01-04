4a59fe81f37eb0dc24c0f22555eff925cb2bcb6e
Martin Hegel <martin.hegel@gmail.com>
2024-01-04 14:06:28 +0100

    Repository erstellt, First Push

---
+++ QuizApp/QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Martin Hegel on 04.01.24.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
